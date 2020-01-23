a8e403d3389f02f0a8ed678210331dc4cf6d0f0d
Bruce <wayne_s@126.com>
2020-01-23 20:56:29 +0800

    update comments

--- NLTK/word_segmentation.py
+++ NLTK/word_segmentation.py
@@ -4,18 +4,21 @@ doc = "Are you curious about tokenization? " \
       "We need to analyze a couple of sentences " \
       "with punctuations to see it in action."
 print(doc)
+# 把样本按句子进行拆分，返回句子列表
 tokens = tk.sent_tokenize(doc)
 for i, token in enumerate(tokens):
     print("%2d" % (i + 1), token)
 
 print('-' * 15)
 
+# 按单词进行拆分，返回单词列表
 tokens = tk.word_tokenize(doc)
 for i, token in enumerate(tokens):
     print("%2d" % (i + 1), token)
 
 print('-' * 15)
 
+# 通过分词器对象按单词进行拆分
 tokenizer = tk.WordPunctTokenizer()
 tokens = tokenizer.tokenize(doc)
 for i, token in enumerate(tokens):
