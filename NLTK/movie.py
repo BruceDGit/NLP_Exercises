4431180cc79c8ffdefd9d01d2a4500ca18b11074
Bruce <wayne_s@126.com>
2020-01-29 23:24:23 +0800

    (sentiment analysis

---
+++ NLTK/movie.py
@@ -0,0 +1,47 @@
+"""
+    情感分析
+"""
+import nltk.corpus as nc
+import nltk.classify as cf
+import nltk.classify.util as cu
+
+
+pdata = []
+fileids = nc.movie_reviews.fileids('pos')
+for fileid in fileids:
+    sample = {}
+    words = nc.movie_reviews.words(fileid)
+    for word in words:
+        sample[word] = True
+    pdata.append((sample, 'POSITIVE'))
+
+ndata = []
+fileids = nc.movie_reviews.fileids('neg')
+for fileid in fileids:
+    sample = {}
+    words = nc.movie_reviews.words(fileid)
+    for word in words:
+        sample[word] = True
+    ndata.append((sample, 'NEGATIVE'))
+
+pnumb, nnumb = int(0.8 * len(pdata)), int(0.8 * len(ndata))
+train_data = pdata[:pnumb] + ndata[:nnumb]
+test_data = pdata[pnumb:] + ndata[nnumb:]
+
+model = cf.NaiveBayesClassifier.train(train_data)
+ac = cu.accuracy(model, test_data)
+print(ac)
+
+reviews = [
+    'It is an amazing movie.',
+    'This is a dull movie. I would never recommend it to anyone.',
+    'The cinematography is pretty great in this movie.',
+    'The direction was terrible and the story was all over the place.']
+sents, probs = [], []
+for review in reviews:
+    sample = {}
+    words = review.split()
+    for word in words:
+        sample[word] = True
+    pcls = model.classify(sample)
+    print(review, '->', pcls)
